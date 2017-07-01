255b8939515fbcd123d6a965294b4cb63f730908
Joel Triemstra <JTriemstra@jtriemstraw7.VML.LAN>
2017-07-01 09:01:28 -0400

    copy/paste location reporting works

--- app/src/main/java/com/example/jtriemstra/quickweather/MainActivity.java
+++ app/src/main/java/com/example/jtriemstra/quickweather/MainActivity.java
@@ -1,22 +1,45 @@
 package com.example.jtriemstra.quickweather;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class MainActivity extends Activity {
+
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("Location", "test");
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        Log.d("Location", "success");
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Log.d("Location", Double.toString(location.getLatitude()));
+                        }
+                    }
+                });
     }
 
     @Override
